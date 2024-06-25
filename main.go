1b132854f92c98ec2589a9648f8abec384a74759
Wellington Lima <wellington.map@hotmail.com>
2024-06-25 10:25:51 -0300

    feat: create opening schema with goorm

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/wellls/opportunities/router"
+import (
+	"github.com/wellls/opportunities/config"
+	"github.com/wellls/opportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+	logger = *config.GetLogger("main")
+
+	//Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	router.Initialize()
 }
